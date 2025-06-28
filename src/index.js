d26ed91b3e84724a1eff6d5df8bf993e93e13291
Unwana Udosen <unwanaudosen8@gmail.com>
2025-06-28 08:54:12 +0100

    Used moment.js to display accurate date and time according to timezone

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-
+      //Montreal
+       function updateMontreal(){
+       let montrealElement = document.querySelector("#montreal");
+       let montrealDateElement = montrealElement.querySelector(".date");
+       let montrealTimeElement = montrealElement.querySelector(".time");
+       
+       let montrealTime = moment().tz("America/Montreal");
+       montrealDateElement.innerHTML = montrealTime.format("MMMM Do, YYYY");
+       
+       montrealTimeElement.innerHTML= montrealTime.format("h:mm:ss [<strong>]A[</strong>]");
+       }
+       updateMontreal();
+       setInterval(updateMontreal, 1000);
+       
+       //Casablanca
+       
+       function updateCasablanca(){
+       let casablancaElement = document.querySelector("#casablanca");
+       let casablancaDateElement = casablancaElement.querySelector(".date");
+       let casablancaTimeElement = casablancaElement.querySelector(".time");
+       
+       let casablancaTime = moment().tz("Africa/Casablanca");
+       casablancaDateElement.innerHTML = casablancaTime.format("MMMM Do, YYYY");
+       
+       casablancaTimeElement.innerHTML= casablancaTime.format("h:mm:ss [<strong>]A[</strong>]");
+       }
+       updateCasablanca();
+       setInterval(updateCasablanca, 1000);
